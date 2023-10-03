aff813f3c693358a68956f48622728a9b1f12afa
mccrush <mccrush2027@gmail.com>
2023-10-03 17:01:21 +0700

    Update get context

--- src/components/BlockCanvas.vue
+++ src/components/BlockCanvas.vue
@@ -4,33 +4,37 @@
 
 <script>
 export default {
+  data() {
+    return {
+      ctx: null
+    }
+  },
   methods: {
     drawInit() {
       console.log('init Draw')
       let canvas = document.getElementById('canvas')
       if (canvas.getContext) {
-        let ctx = canvas.getContext('2d')
+        this.ctx = canvas.getContext('2d')
         //ctx.transform(1, 0, 0, -1, 0, canvas.height)
-        return ctx
+        //return ctx
       }
     },
     drawRect({ x, y, w, h }) {
       console.log('drawRect in canvas')
-      let ctx = this.drawInit()
-      ctx.strokeRect(x, y, w, h)
+      if (!this.ctx) {
+        this.drawInit()
+      }
+      this.ctx.strokeRect(x, y, w, h)
     },
     drawCurve({ cp1x, cp1y, cp2x, cp2y, x, y }) {
-      console.log('drawRect in canvas')
-      let ctx = this.drawInit()
-      ctx.beginPath()
-      ctx.moveTo(100, 75)
-      ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y)
-
-      //ctx.quadraticCurveTo(cpx, cpy - 100, x - 25, y - 50)
-
-      //ctx.moveTo(125, 125)
-      //ctx.quadraticCurveTo(cpx, cpy, x, y)
-      ctx.stroke()
+      console.log('drawCurve in canvas')
+      if (!this.ctx) {
+        this.drawInit()
+      }
+      this.ctx.beginPath()
+      this.ctx.moveTo(100, 75)
+      this.ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y)
+      this.ctx.stroke()
     }
   }
 }
